5f54892b8dcc72b765d25bb2605986ff51173470
Pavel Anni <pavel.anni@gmail.com>
2015-07-23 21:26:53 -0400

    Edited the functions and added some comments The function doesn't seem to do what expected I'll continue working on it.

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This two functions (should) reverse a matrix 
+## but they use a trick: if the matrix was already 
+## reversed before, the second function uses the cached 
+## answer to save time. If not, it reverses the matrix normally
 
-## Write a short comment describing this function
+## The following function creates a list of functions
+## to get matrix, set matrix, set inversed matrix and 
+## get inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setsolve <- function(solve) m <<- solve
-  getsolve <- function() x
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() x
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function checks if the cached reversed matrix exists
+## and if it does, returns the cached result. If it doesn't-
+## it reverses the matrix the usual way
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
